5454a77a2a1f952f180f1dc9a7fdda4f6de76719
Pushkaraj Lolusare <pushkarajlolusare24@gmail.com>
2023-07-12 14:37:03 +0530

    ok

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,33 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const mongoose = require("mongoose");
 require("dotenv").config();
 const connection = mongoose.connect(process.env.db_url, {
